b9b77216a76d119673d477b64f95928ccd54a948
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2022-11-03 19:18:41 +0900

    :sparkles: エラーハンドリングの基盤を追加

--- src/main/kotlin/com/restful/api/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/restful/api/exception/ExceptionHandler.kt
@@ -1,12 +1,32 @@
 package com.restful.api.exception
 
+import com.restful.api.dto.ErrorResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
+@RestControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
 
-  @ExceptionHandler(BadRequestException::class)
-  fun handleBadRequestException(exception: BadRequestException) {
+  @ExceptionHandler(NotFoundException::class)
+  fun handleNotFoundException(exception: NotFoundException, request: WebRequest): ResponseEntity<Any> {
+    val error = ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.message)
+    return super.handleExceptionInternal(exception, error, HttpHeaders(), HttpStatus.NOT_FOUND, request)
+  }
 
+  @ExceptionHandler(BadRequestException::class)
+  fun handleBadRequestException(exception: BadRequestException, request: WebRequest): ResponseEntity<Any> {
+    val error = ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.message)
+    return super.handleExceptionInternal(exception, error, HttpHeaders(), HttpStatus.BAD_REQUEST, request)
+  }
+
+  @ExceptionHandler(Exception::class)
+  fun handleAllException(exception: Exception, request: WebRequest): ResponseEntity<Any> {
+    val error = ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.message)
+    return super.handleExceptionInternal(exception, error, HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request)
   }
 }
