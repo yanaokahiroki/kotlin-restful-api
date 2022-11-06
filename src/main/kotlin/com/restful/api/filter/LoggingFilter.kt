fca5bb8e4069fd3bc552788b6106388325144e88
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2022-11-06 15:11:32 +0900

    :sparkles: Filterを導入

---
+++ src/main/kotlin/com/restful/api/filter/LoggingFilter.kt
@@ -0,0 +1,27 @@
+package com.restful.api.filter
+
+import org.slf4j.LoggerFactory
+import org.springframework.util.StopWatch
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class LoggingFilter : OncePerRequestFilter() {
+  private val log = LoggerFactory.getLogger(LoggingFilter::class.java)
+
+  override fun doFilterInternal(
+    request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+  ) {
+    val stopWatch = StopWatch()
+    stopWatch.start()
+
+    filterChain.doFilter(request, response)
+
+    stopWatch.stop()
+
+    log.info(
+      "${request.method}\t${request.requestURI}\t${response.status}\t${stopWatch.totalTimeMillis}ms"
+    )
+  }
+}
